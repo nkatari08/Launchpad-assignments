4bd081c3ebadb345d91a4c3cce2430aa4bfa14a6
nkatari08 <41177877+nkatari08@users.noreply.github.com>
2020-02-26 23:35:57 +0530

    Add files via upload

---
+++ assignment1_1.py
@@ -0,0 +1,5 @@
+name= input("enter your name ")
+age=int(input("enter your age"))
+current_year=2020
+age_of_birth=current_year-age
+print("hey"+name+" you will be 100 years in the year", age_of_birth+100)
